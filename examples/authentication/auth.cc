d22a851e4a3bdfc2f12b505442b2da77d573a6ae
André L. C. Moreira <alcm99@gmail.com>
2021-09-11 11:56:06 -0300

    [ESPOTIFAI-API #X] adding authentication example

---
+++ examples/authentication/auth.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+#include "access_listener.h"
+#include "api.h"
+
+class EventHandler : public espotifai_api::AccessListener {
+ public:
+  void OnAccessGuaranteed(const std::string &token) const override {
+    std::cout << "access guaranteed, token: " << token << std::endl;
+  }
+
+  void OnAccessDenied(const std::string &msg) const override {
+    std::cout << "access denied!" << std::endl;
+  }
+};
+
+int main(int argc, char *argv[]) {
+  if (argc != 3) {
+    std::cout << "usage: " << argv[0] << " client_id client_secret"
+              << std::endl;
+    std::exit(1);
+  }
+
+  const std::string kClientId{argv[1]};
+  const std::string kClientSecret{argv[2]};
+
+  espotifai_api::Api api;
+  EventHandler ev;
+
+  api.RequestAccess(ev, kClientId, kClientSecret);
+
+  std::exit(0);
+}
